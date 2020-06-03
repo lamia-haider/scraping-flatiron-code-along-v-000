a33dca9e81fc147939aca3aa334be89c60b83afc
Lamia Haider <lamia.haider@gmail.com>
2020-06-03 21:47:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,11 +7,12 @@ require_relative "./course.rb"
 class Scraper
 
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
   end
 
   def get_courses
+    self.get_page.css(".post")
 
   end
 
